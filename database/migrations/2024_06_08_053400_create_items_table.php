4046d25909609b9940490acf1813d3acdbedfa37
TheVarja <40161420+TheVarja@users.noreply.github.com>
2024-06-22 21:44:53 +0700

    Adding Dashboard

--- database/migrations/2024_06_08_053400_create_items_table.php
+++ database/migrations/2024_06_08_053400_create_items_table.php
@@ -1,21 +1,52 @@
 <?php
+//
+//use Illuminate\Database\Migrations\Migration;
+//use Illuminate\Database\Schema\Blueprint;
+//use Illuminate\Support\Facades\Schema;
+//
+//return new class extends Migration
+//{
+//    /**
+//     * Run the migrations.
+//     */
+//    public function up(): void
+//    {
+//        Schema::create('items', function (Blueprint $table) {
+//            $table->id();
+//            $table->integer('user_id')->index();
+//            $table->string('name');
+//            $table->integer('stock', 1,0 );
+//            $table->float('price', 8, 2);
+//            $table->boolean('status');
+//            $table->timestamps();
+//        });
+//    }
+//
+//    /**
+//     * Reverse the migrations.
+//     */
+//    public function down(): void
+//    {
+//        Schema::dropIfExists('items');
+//    }
+//};
+
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('items', function (Blueprint $table) {
-            $table->id();
+            $table->id();  // Primary key
             $table->integer('user_id')->index();
             $table->string('name');
-//            $table->integer('stock', 1,0 );
+            $table->integer('stock');  // Regular integer column
             $table->float('price', 8, 2);
             $table->boolean('status');
             $table->timestamps();
